b9766d2aeb7ae78abef69629bdf3674ae94cd56c
“LJYang02” <1296393074@qq.com>
2022-11-21 20:10:57 +0800

    add one java

---
+++ code_test_add.java
@@ -0,0 +1,9 @@
+public class helloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("hello world1");
+        System.out.println("hello world2");
+        System.out.println("我被修改了");
+    }
+
+}
