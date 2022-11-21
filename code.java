d274eb7e2dfb69fe9fbf8272a953c6ba6a87a798
“LJYang02” <1296393074@qq.com>
2022-11-21 20:32:04 +0800

    test2 modified

--- code.java
+++ code.java
@@ -4,6 +4,8 @@ public class helloWorld {
         System.out.println("hello world1");
         System.out.println("hello world2");
         System.out.println("我被修改了");
+
+        string obj="我在test2分支中被修改了，完成了提交";
     }
 
 }
